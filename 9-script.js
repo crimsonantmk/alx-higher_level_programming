d507c0790d38086bb9dd5330ee9a9c9e3abb3101
Antony Munyiri <144614414+crimsonantmk@users.noreply.github.com>
2024-05-30 23:32:27 +0300

    Delete 9-script.js

--- 9-script.js
+++
@@ -1,7 +0,0 @@
-const $ = window.$;
-$(window).bind('load', function () {
-  const url = 'https://fourtonfish.com/hellosalut/?lang=fr';
-  $.getJSON(url, function (result) {
-    $('DIV#hello').append(result.hello);
-  });
-});
