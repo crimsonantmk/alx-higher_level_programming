fed8888e04c2822bbe3a6a2e2a9f2f4e67c2ff9a
Antony Munyiri <144614414+crimsonantmk@users.noreply.github.com>
2024-05-30 23:33:21 +0300

    Delete 8-script.js

--- 8-script.js
+++
@@ -1,8 +0,0 @@
-const $ = window.$;
-const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
-$.getJSON(url, function (body) {
-  const result = body.results;
-  $.each(result, function (i, field) {
-    $('UL#list_movies').append('<li>' + field.title + '</li>');
-  });
-});
